47323a8fc6c6938001e6617f1b279b866b7242ed
Marco Bareno <mbareno@technisys.net>
2020-04-20 16:48:50 -0300

    primer cambio, adaptacion del fork de gus, se reemplazan variables y se cambia el color del boton search

--- albun.js
+++ albun.js
@@ -1,8 +1,12 @@
 /*este array será en el futuro una lista heroes*/
-
-[{nombre:"Thor"},{nombre:"Capitan America"},{nombre:"Hulk"}].forEach((heroe)=>{
-    
-    document.querySelector(".row").innerHTML=`${document.querySelector(".row").innerHTML}
+let elRow = document.querySelector(".row");
+const heroes = [
+  { nombre: "Thor" },
+  { nombre: "Capitan America" },
+  { nombre: "Hulk" },
+]
+heroes.forEach(
+  (heroe) => {elRow.innerHTML = `${elRow.innerHTML}
     <div class="col-md-4">
     <div class="card mb-4 shadow-sm">
     <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
@@ -17,9 +21,6 @@
         </div>
     </div>
     </div>
-    </div>`
-
-})
-
-
-
+    </div>`;
+  }
+);
